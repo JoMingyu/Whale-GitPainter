a92dda18f2e674e182ec59d01a712e481e25bf60
JoMingyu <city7310@naver.com>
2017-07-14 15:19:20 +0900

    profile/repo 판별

--- project/background.js
+++ project/background.js
@@ -3,7 +3,17 @@ $(document).ready(function() {
         let url = tab.url;
         if(url !== undefined && changeInfo.status == "complete") {
             // 여러번 디스패치되기 때문에 리스너가 자주 호출됨
-            alert("changed");
+            let backOfUrl = url.split('github.com/')[1];
+            
+            if(backOfUrl.length > 0) {
+                if(backOfUrl.split('/')[1] === undefined) {
+                    // Profile
+                    alert('profile');
+                } else {
+                    // Repository
+                    alert('repo');
+                }
+            }
         }
     });
 });
