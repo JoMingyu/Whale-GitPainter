6208a0992ab156313e115a8353f83c26c12dd0d2
JoMingyu <city7310@naver.com>
2017-07-15 00:49:36 +0900

    saveColor, loadColor
    load가 제대로 되질 않는다

--- project/js/background.js
+++ project/js/background.js
@@ -1,12 +1,14 @@
-$(document).ready(function() {
-    whale.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+$(document).ready(function () {
+    whale.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        saveColor('1', '#asdasd000');
+        loadColor('1');
         let url = tab.url;
-        if(url !== undefined && changeInfo.status == "complete") {
+        if (url !== undefined && changeInfo.status == "complete") {
             // 여러번 디스패치되기 때문에 리스너가 자주 호출됨
             let backOfUrl = url.split('github.com/')[1];
-            
-            if(backOfUrl.length > 0) {
-                if(backOfUrl.split('/')[1] === undefined) {
+
+            if (backOfUrl.length > 0) {
+                if (backOfUrl.split('/')[1] === undefined) {
                     // Profile
                     paint();
                 } else {
@@ -18,13 +20,20 @@ $(document).ready(function() {
 });
 
 function paint() {
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         $('.contrib-legend .legend li').eq(4 - i).css('background-color', '#ffffff');
     }
 }
 
-function saveColor(position, colorCode) {
-    whale.storage.sync.set({position: colorCode}, () => {
-        alert('Saved');
+function saveColor(key, val) {
+    whale.storage.local.set({key: val}, () => {
+
+    });
+}
+
+function loadColor(key) {
+    whale.storage.local.get(key, result => {
+        alert('load complete');
+        alert(result[0]);
     });
 }
