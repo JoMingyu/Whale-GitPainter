bf911dcbec46c5a1da82f882276279355461531c
JoMingyu <city7310@naver.com>
2017-07-16 02:17:45 +0900

    앙 고쳤다 기모링~

--- project/js/colormanager.js
+++ project/js/colormanager.js
@@ -1,25 +1,34 @@
 let contribColors = ['#eee', '#c6e48b', '#7bc96f', '#239a3b', '#196127'];
 
 function loadContribColors() {
-    whale.storage.sync.get('contrib', items => {
-        alert(items);
-
-        if (items === undefined) {
-            
-        } else {
-
+    whale.storage.sync.get(undefined, items => {
+        for (let i = 1; i <= 5; i++) {
+            let colorCode = items[i.toString()];
+            if (colorCode !== undefined) {
+                contribColors[i - 1] = colorCode;
+            }
         }
     });
 }
 
 function changeContribColor(position, colorCode) {
-    whale.storage.sync.set({'position': position, 'code': code}, () => {
-        
+    //// logic
+    whale.storage.sync.set({ [position]: colorCode }, () => {
+        // https://stackoverflow.com/questions/11692699/chrome-storage-local-set-using-a-variable-key-name
     });
+
+    //// junk
+    // whale.storage.sync.get(undefined, items => {
+    //     alert(items[position]);
+    // });
 }
 
 whale.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    for(let i = 0; i < 5; i++) {
-        $('.contrib-legend .legend li').eq(i).css('background-color', contribColors[i]);
-    }
+    //// junk
+    // changeContribColor(1, '#ccccasdasdf');
+
+    //// logic
+    // for(let i = 0; i < 5; i++) {
+    //     $('.contrib-legend .legend li').eq(i).css('background-color', contribColors[i]);
+    // }
 });
