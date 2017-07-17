de430fe35ad339a617ee1889c7d3174e015d8769
JoMingyu <city7310@naver.com>
2017-07-17 10:54:20 +0900

    junk 제거

--- project/js/colormanager.js
+++ project/js/colormanager.js
@@ -1,6 +1,6 @@
 let contribColors = ['#eee', '#c6e48b', '#7bc96f', '#239a3b', '#196127'];
 
-function loadContribColors() {
+function loadContribColors(cb) {
     whale.storage.sync.get(undefined, items => {
         for (let i = 1; i <= 5; i++) {
             let colorCode = items[i.toString()];
@@ -8,27 +8,21 @@ function loadContribColors() {
                 contribColors[i - 1] = colorCode;
             }
         }
+
+        cb();
     });
 }
 
 function changeContribColor(position, colorCode) {
-    //// logic
     whale.storage.sync.set({ [position]: colorCode }, () => {
         // https://stackoverflow.com/questions/11692699/chrome-storage-local-set-using-a-variable-key-name
     });
-
-    //// junk
-    // whale.storage.sync.get(undefined, items => {
-    //     alert(items[position]);
-    // });
 }
 
 whale.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    //// junk
-    // changeContribColor(1, '#ccccasdasdf');
-
-    //// logic
-    // for(let i = 0; i < 5; i++) {
-    //     $('.contrib-legend .legend li').eq(i).css('background-color', contribColors[i]);
-    // }
+    loadContribColors(() => {
+        for (let i = 0; i < 5; i++) {
+            $('.contrib-legend .legend li').eq(i).css('background-color', contribColors[i]);
+        }
+    });
 });
