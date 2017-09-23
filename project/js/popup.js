78acc97973b2a964542e78ec49857e9d101c3fea
AshtonYoon <y109384@naver.com>
2017-09-23 19:54:23 +0900

    apply progress bar

--- project/js/popup.js
+++ project/js/popup.js
@@ -251,6 +251,15 @@ app
         }
     });
 
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+    
+    function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
     function hexToRgb(hex) {
         // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
         var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
